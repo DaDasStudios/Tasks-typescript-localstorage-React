a2b10404879ae56671edacb93d669a346ccef90a
David <dadastudios382@gmail.com>
2022-06-25 08:27:52 -0500

    correct base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/typescript-tasks-app-react-localstorage/"
+  base: "/dadasstudios/typescript-tasks-app-react-localstorage/"
 })
